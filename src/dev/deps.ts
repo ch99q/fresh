4d6e767e2386922073377d76a45eca7c3903cad6
Christian Hansen <64793a1a@gmail.com>
2022-09-08 08:04:45 +0200

    feat: added relative and separator imports

--- src/dev/deps.ts
+++ src/dev/deps.ts
@@ -5,7 +5,9 @@ export {
   fromFileUrl,
   join,
   resolve,
+  relative,
   toFileUrl,
+  SEP,
 } from "https://deno.land/std@0.150.0/path/mod.ts";
 export { walk } from "https://deno.land/std@0.150.0/fs/walk.ts";
 export { parse } from "https://deno.land/std@0.150.0/flags/mod.ts";
